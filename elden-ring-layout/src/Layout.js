c241d251506527a3d892e1255542841b4582ec9b
JacobTipps43 <jacobtippett43@gmail.com>
2024-11-05 13:12:23 -0500

    Pushing to try and fix

--- elden-ring-layout/src/Layout.js
+++ elden-ring-layout/src/Layout.js
@@ -6,20 +6,20 @@ const Layout = () => {
   return (
     <>
     <nav id="main-nav">
+    <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/">Stats</Link></li>
         <li><Link to="/">Weapons</Link></li>
         <li><Link to="/">Armor</Link></li>
         <li><Link to="/">Talismans</Link></li>
         <li><Link to="/">Location</Link></li>
+    </ul>
     </nav>
 
     <Outlet />
 
     <p>This is my footer</p>
     </>
-
-
   );
 };
 
