b7e2b99fb01c78d6566b659214c8ec5fbf309303
JacobTipps43 <jacobtippett43@gmail.com>
2024-11-06 11:33:47 -0500

    Adding stuff

--- elden-ring-layout/src/Layout.js
+++ elden-ring-layout/src/Layout.js
@@ -1,18 +1,20 @@
 import "./css/Layout.css";
+import Header from "./components/header";
 import {Outlet,Link } from "react-router-dom";
 import React from "react";
 
 const Layout = () => {
   return (
     <>
+    <Header />
     <nav id="main-nav">
-    <ul>
-        <li><Link to="/home">Home</Link></li>
+    <ul className="columns">
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/stats">Stats</Link></li>
         <li><Link to="/weapons">Weapons</Link></li>
         <li><Link to="/armor">Armor</Link></li>
         <li><Link to="/talismans">Talismans</Link></li>
-        <li><Link to="/locations">Location</Link></li>
+        <li><Link to="/location">Location</Link></li>
     </ul>
     </nav>
 
